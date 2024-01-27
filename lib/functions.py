c1bbf6973fd45d28268a161fee7fc71b5a988df0
Yisel Cintora Vega <yiselcintoravega@gmail.com>
2024-01-27 16:11:03 -0500

    completed lab

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,33 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
-    pass
+    print("Hello, programmer!")
+
 
 def greet(name):
-    pass
+    print (f"Hello, {name}!")
 
-def greet_with_default(name="programmer"):
-    pass
+
+def greet_with_default(name = "programmer"):
+    print(f'Hello, {name}!')
+
 
 def add(num1, num2):
-    pass
+  return num1 + num2
+
 
 def halve(number):
-    pass
+  
+  return ( number / 2)
+
+
+
+
+def my_function(param):
+    print("Running my_function")
+    return param + 1
+
+# New code goes here!
+my_function_return_value = my_function(1)
+
+my_function_return_value
